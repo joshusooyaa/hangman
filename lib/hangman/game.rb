6bd097cdeb8ca926ebcc68df022b4ee79453f7eb
Josh Sawyer <thejoshsawyer@gmail.com>
2023-07-07 12:27:19 -0700

    Add random word method

---
+++ lib/hangman/game.rb
@@ -0,0 +1,12 @@
+class Game
+  def initialize
+    @word = word
+    @guesses_left = guesses_left
+    @guessed_letters = guessed_letters
+    @game_state = game_state
+  end
+
+  # def self.create_game
+
+  # end
+end
